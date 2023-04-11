a2ba230c8b9d572723ac647ca303c0ae4a0129fe
jeyofdev <jgregoire.pro@gmail.com>
2023-04-11 17:37:05 +0200

    init project react with vite and typescript

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+const App = () => {
+	return (
+		<div
+			style={{
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				minHeight: '100vh',
+				width: '100%',
+			}}
+		>
+			<h1>Hello world !!!</h1>
+		</div>
+	);
+};
+
+export default App;
